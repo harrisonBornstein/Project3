58764f0f6729551b02d8a43454658abdaeadedc1
kevinten11 <kreimnitz@ou.edu>
2013-10-10 13:29:22 -0500

    Test

---
+++ Project2Final/src/AuthorTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class AuthorTest {
+
+	
+	@Test
+	public void testAuthor() 
+	{
+		List<String> titles = new ArrayList<String>();
+		titles.add("Cool Book");
+		
+		Author me = new Author("Kevin", "Reimnitz", titles);
+		Assert.assertEquals(me.getFirstName(), "Kevin");
+		Assert.assertEquals(me.getLastName(), "Reimnitz");
+		Assert.assertEquals(me.getListOfPaperTitles(), titles);
+	}
+
+
+	
+
+}
